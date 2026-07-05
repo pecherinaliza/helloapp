81e1097c6dab55e85984d3e04961ee6537be1014
black <black@example.com>
2026-07-05 18:56:46 +0300

    Call greetWhite and greetBlack

--- project.py
+++ project.py
@@ -2,8 +2,14 @@ def say_hello():
     print("hello everyone")
 
 
+def greetWhite():
+    print("hello white")
+
+
 def greetBlack():
     print("hello black")
 
 
 say_hello()
+greetWhite()
+greetBlack()
